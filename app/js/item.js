5a5f0f533ef82d88baad94f01ac5da4e0ef863b8
Victor Kaike Cezemer Peixoto <victor.peixoto@consulfarmasaude.com.br>
2018-06-21 13:26:16 -0300

    iniciando o css do item

--- app/js/item.js
+++ app/js/item.js
@@ -1,8 +1,19 @@
-window.onload = function () {
-    $.getJSON("../data/item.json", function (data) {
-        console.log(data)
-    });
-};
+function criaDivMod(mod){
+    let div = "<div class='container-wrap'>"
+    
+        $( "<div/>", {
+            "class": "div-mod",
+            "id": "id-"+mod
+        }).appendTo($('#conteudo-item-dialog'));
+
+        $( "<span/>", {
+            "class": "texto-mod",
+            text: mod
+        }).appendTo($('#id-'+mod));;
+}
 
 $(document).ready(function () {
+    $.getJSON("../data/item.json", function (data) {
+        criaDivMod('TESTE');
+    });
 });
